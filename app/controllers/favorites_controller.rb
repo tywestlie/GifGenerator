3940cda7248c65ea16f96f39199fa0e4be92f20a
Tyler Westlie <tyler.westlie@gmail.com>
2018-05-22 19:58:54 -0600

    still working on favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,25 @@
 class FavoritesController < ApplicationController
 
   def new
+    @user = User.find(params[:user_id])
+    @gif = Gif.find(params[:gif_id])
+    @favorite = Favorite.new
+    require 'pry'; binding.pry
+    redirect_to gifs_path
   end
 
   def create
+    @user = User.find(params[:user_id])
+    favorite = @user.favorites.create(favorite_params)
+    if favorite.save
+      flash[:success] = 'Added to Favorites!'
+    end
+    redirect_to gifs_path
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:user_id, :gif_id)
   end
 end
